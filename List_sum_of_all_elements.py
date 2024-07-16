1bdaf670d5d74e9d0afe11c606da2b82aa28c365
Dev Dutt <30376826+duttdev489@users.noreply.github.com>
2024-07-16 11:50:45 +1000

    Create List_sum_of_all_elements.py

---
+++ List_sum_of_all_elements.py
@@ -0,0 +1,5 @@
+#Isha is assigned a task to calculate the total amount at the end of the day. Provided with a list, write a Python program for Isha to sum all the elements in the list.
+# Take the list as input from the user
+lst =eval(input()) 
+result =sum(lst)
+print(result)
